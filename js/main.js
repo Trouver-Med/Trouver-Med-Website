ed3dc8f3e7b18bc509b4dffd0d5c38b1df94f4bc
London-Joe <jaylondon072@gmail.com>
2023-02-10 02:38:23 +0100

    fixed the navbar bugs

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.nav_bar');
+const navlink = document.querySelectorAll('.nav_link');
 hamburger.onclick = () => {
  hamburger.classList.toggle('active');
  navbar.classList.toggle('active');
 }
+
+navlink.forEach(n => n.addEventListener('click', () => {
+ hamburger.classList.remove('active');
+ navbar.classList.remove('active');
+}))
