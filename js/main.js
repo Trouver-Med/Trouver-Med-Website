97093b9352a9dd0f11aec636a5a36dc450e64ca9
London-Joe <jaylondon072@gmail.com>
2023-02-13 21:30:08 +0100

    Creating carousel slide and fadeUp animation

--- js/main.js
+++ js/main.js
@@ -2,11 +2,29 @@ const hamburger = document.querySelector('.hamburger');
 const navbar = document.querySelector('.nav_bar');
 const navlink = document.querySelectorAll('.nav_link');
 hamburger.onclick = () => {
- hamburger.classList.toggle('active');
- navbar.classList.toggle('active');
+ hamburger.classList.toggle('open');
+ navbar.classList.toggle('open');
 }
 
 navlink.forEach(n => n.addEventListener('click', () => {
- hamburger.classList.remove('active');
- navbar.classList.remove('active');
+ hamburger.classList.remove('open');
+ navbar.classList.remove('open');
 }))
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+ let reveals = document.querySelectorAll('.reveal');
+
+ for(let i = 0; i < reveals.length; i++) {
+  let windowHeight = window.innerHeight;
+  let revealtop = reveals[i].getBoundingClientRect().top;
+  let revealpoint = 150;
+
+  if(revealtop < windowHeight - revealpoint) {
+   reveals[i].classList.add('showup');
+  }else {
+   reveals[i].classList.remove('showup');
+  }
+ }
+}
